cc068ec5eb02cf26ef375691c0233656db9efe93
BIPIN GHIMIRE <94663552+beepin6409@users.noreply.github.com>
2023-08-13 11:50:29 +0530

    Add files via upload

---
+++ model/predict.py
@@ -0,0 +1,47 @@
+import joblib
+import sys
+import json
+import wikipedia as wiki
+import ctypes
+import pandas as pd
+
+
+disease_prevention=pd.read_csv('disease_prevention.csv')
+disease_list=disease_prevention[["Disease"]]
+disease=disease_list.values.tolist()
+
+prevention_list=disease_prevention[["Prevention"]]
+prevention=prevention_list.values.tolist()
+
+
+def predict_disease(object):
+    variable=model.predict(object)[0]
+    return disease[variable],prevention[variable]
+
+
+model=joblib.load('model.pkl')
+
+
+
+
+
+
+input_data_array=[[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+prediction,prevention=predict_disease(input_data_array)
+
+word=prediction[0]
+info=wiki.summary(word, sentences=4)
+
+
+
+print(json.dumps(prediction))
+print(json.dumps(info))
+print(json.dumps(prevention))
+
+
+
+
+
+
+
